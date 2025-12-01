4adcb647b01a8095f1cfa4dd9c05e17faf8adc89
Roman Fedoriv <fedoriv.roman.v@gmail.com>
2025-12-01 15:06:12 +0200

    fix validation

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -6,7 +6,7 @@ export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid(...TAGS),
+    tag: Joi.string().valid(...TAGS).optional(),
     search: Joi.string().trim().allow(''),
   }),
 };
@@ -23,12 +23,12 @@ export const noteIdSchema = {
   }),
 };
 
-export const createNoteSchema =  {
-   [Segments.BODY]:Joi.object({
-  title: Joi.string().min(1),
-  content: Joi.string().allow(''),
-  tag: Joi.string().valid(...TAGS),
-}),
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).required(),
+    content: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS).optional(), 
+  }),
 };
 
 export const updateNoteSchema = {
@@ -39,5 +39,5 @@ export const updateNoteSchema = {
     title: Joi.string().min(1),
     content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
-  }).min(1),
+  }).min(1), // Мінімум одне поле для оновлення
 };
