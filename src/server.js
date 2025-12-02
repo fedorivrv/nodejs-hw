aff6b84e4da8a930ca497a5f2e8f3792cbf92689
Roman Fedoriv <fedoriv.roman.v@gmail.com>
2025-12-02 18:21:40 +0200

    done

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ import cookieParser from 'cookie-parser';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
@@ -22,6 +24,8 @@ app.use(cookieParser());
 
 /// Маршрути
 app.use(notesRoutes);
+app.use(authRoutes);
+app.use(userRoutes);
 
 // Middleware 404
 app.use(notFoundHandler);
